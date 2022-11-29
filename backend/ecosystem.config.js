9ee3db3323ce39a3e3095628660eeb6797196cd7
Apple <IIKolchin@yandex.ru>
2022-11-29 17:39:29 +0300

    change config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,30 +1,6 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './.env.deploy' });
-
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REPO,
-  DEPLOY_PATH_ENV,
-  DEPLOY_REF = 'origin/main',
-} = process.env;
-
-export const apps = [
+module.exports = [
   {
-    name: 'api-service',
     script: '/app/dist/main.js',
+    name: 'api-service',
   },
 ];
-export const deploy = {
-  production: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: DEPLOY_REPO,
-    path: DEPLOY_PATH,
-    'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_ENV}`,
-    'post-deploy': 'docker compose up --build -d',
-  },
-};
